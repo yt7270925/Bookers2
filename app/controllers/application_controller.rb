1138d5ad2c596426ffaf454768628b113fd5ab84
EC2 Default User <ec2-user@ip-172-31-16-77.ec2.internal>
2022-06-25 02:50:34 +0000

    サインイン、サインアウト後の遷移先の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,17 @@ class ApplicationController < ActionController::Base
   # ログインで使う値から外したemailを許可する
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # サインイン後の遷移先
+  def after_sign_in_path_for(resource)
+    about_path #後でBookのindexに変える
+  end
+
+  # ログアウト後の遷移先 デフォルトはルートパスであるから後で消す
+  def after_sign_out_path_for(resource)
+    about_path #後でトップページに変える
+    # root_path
+  end
+
   private
 
   def configure_permitted_parameters
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -59,12 +59,12 @@ Devise.setup do |config|
 
   # Configure which authentication keys should be case-insensitive.
   # These keys will be downcased upon creating or modifying a user and when used
-  # to authenticate or find a user. Default is :email.
+  # to authenticate or find a user. Default is :name.
   config.case_insensitive_keys = [:email]
 
   # Configure which authentication keys should have whitespace stripped.
   # These keys will have whitespace before and after removed upon creating or
-  # modifying a user and when used to authenticate or find a user. Default is :email.
+  # modifying a user and when used to authenticate or find a user. Default is :name.
   config.strip_whitespace_keys = [:email]
 
   # Tell if authentication through request.params is enabled. True by default.
