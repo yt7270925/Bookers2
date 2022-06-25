743fc1ac2b2d1a82269aa41a23f857c450629133
EC2 Default User <ec2-user@ip-172-31-16-77.ec2.internal>
2022-06-25 04:04:20 +0000

    create機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,28 @@
 class BooksController < ApplicationController
+
+  # def new
+  #   @book = Book.new
+  # end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path #showへのリダイレクト
+  end
+
   def index
+    @book = Book.new
   end
 
   def show
   end
+
+  # ストロングパラメータ
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get 'homes/about', as: 'about'
 
-  resources :books, only: [:new, :index, :show]
+  resources :books, only: [:new, :index, :show, :create]
 
 end
